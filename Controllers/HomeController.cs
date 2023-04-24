abfb1f6224e4200fed432ec8d375b199ba8003ff
rzamirzayev <rzatm@code.edu.az>
2023-04-24 17:02:14 +0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Eterna.DataAccess;
+using Eterna.Models;
+using Eterna.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Eterna.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly DataContext _dataContext;
+
+        public HomeController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Slide> slides = _dataContext.Slides.ToList();
+
+            HomeViewModel vm = new HomeViewModel
+            {
+                Services = Data.Services,
+                Features = Data.Features,
+                Slides = slides
+            };
+
+            return View(vm);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Slide.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eterna.Models
+{
+    public class Slide
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Desc { get; set; }
+
+    }
+}
